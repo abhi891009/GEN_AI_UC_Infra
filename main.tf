0016a443ae05fdd4b0f606b33fc8dd7dda86fc65
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-06-18 08:02:55 +0000

    Creating infra

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ module "ecs" {
   source             = "./modules/ecs"
   app_name           = "myapp"
   cluster_name       = "myapp-cluster"
-  image_url          = "144317819575.dkr.ecr.us-east-1.amazonaws.com/appointment_ecr"
+  image_url          = "921666318654.dkr.ecr.us-east-1.amazonaws.com/myapp-repo"
   execution_role     = module.iam.execution_role_arn
-  subnet_ids         = ["subnet-08c0bca341b42e03b", "subnet-0431e51cc9cb6409c"]
-  security_group_id  = "sg-0e0a91c556b78754d"
+  subnet_ids         = ["subnet-0f2bab7ec574c5b1b", "subnet-01b627d11039e9c94"]
+  security_group_id  = "sg-039934fef95c7b06a"
 }
