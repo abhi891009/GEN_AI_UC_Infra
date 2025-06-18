260474b620a148809c3a12f805fa6bae89b231a2
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-06-18 07:51:42 +0000

    Creating infra

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-   bucket       = "abhi100bucket121"
+   bucket       = "abhi100bucket1211"
    key          = "path/to/state/Ml-ops"
    region       = "us-east-1"
    use_lockfile = true
